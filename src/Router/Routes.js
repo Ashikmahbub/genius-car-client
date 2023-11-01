ebb35bab539ca10d558f062e0fde66382559d798
Ashikmahbub <ashikmahbub56@gmail.com>
2023-11-01 08:56:40 +0600

    first commit

---
+++ src/Router/Routes.js
@@ -0,0 +1,9 @@
+// ****router setup****
+import { RouterProvider, createBrowserRouter } from "react-router-dom";
+const router = createBrowserRouter([
+    {
+      path : '/',
+
+    }
+  ])
+  export default router;
