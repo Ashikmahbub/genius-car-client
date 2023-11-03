38394e7750a8ed441841ef1efa21c8b7bbdf3d8f
Ashikmahbub <ashikmahbub56@gmail.com>
2023-11-03 12:33:30 +0600

    second commit

---
+++ src/Pages/About/About.js
@@ -0,0 +1,34 @@
+import React from "react";
+import person from "../../assets/images/about_us/person.jpg";
+import parts from "../../assets/images/about_us/parts.jpg";
+
+const About = () => {
+  return (
+    <div>
+      <div className="hero min-h-screen bg-base-200">
+        <div className="hero-content flex-col lg:flex-row">
+          <div className=" relative w-1/2">
+            <img src={person} alt="" className="rounded-lg shadow-2xl w-4/5 h-full" />
+            <img src={parts} alt="" className="rounded-lg shadow-2xl absolute w-3/5 right-5 top-1/2 border-8" />
+          </div>
+          <div>
+            <p className="text-2xl text-orange-600 font-bold">About Us</p>
+            <h1 className="text-5xl font-bold w-1/2">
+              We are qualified & of experience in this field
+            </h1>
+            <p className="py-6">
+              Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda
+              excepturi exercitationem quasi. In deleniti eaque aut repudiandae
+              et a id nisi.
+            </p>
+            <button className="btn btn-primary bg-orange-600">
+              Get More Info
+            </button>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default About;
--- src/Router/Routes.js
+++ src/Router/Routes.js
@@ -1,8 +1,17 @@
 // ****router setup****
 import { RouterProvider, createBrowserRouter } from "react-router-dom";
+import Main from "../Layout/Main";
+import Home from "../Pages/Home/Home";
 const router = createBrowserRouter([
     {
       path : '/',
+      element : <Main></Main>,
+      children : [
+        {
+            path: '/',
+            element : <Home></Home>
+        }
+      ]
 
     }
   ])
