046ae665300317e9527a5f2ef09a12fb4e0c3c4f
Ashikmahbub <ashikmahbub56@gmail.com>
2023-11-08 14:37:43 +0600

    second commit

--- src/Pages/About/About.js
+++ src/Pages/About/About.js
@@ -5,14 +5,14 @@ import parts from "../../assets/images/about_us/parts.jpg";
 const About = () => {
   return (
     <div>
-      <div className="hero min-h-screen bg-base-200">
+      <div className="hero my-20">
         <div className="hero-content flex-col lg:flex-row">
           <div className=" relative w-1/2">
             <img src={person} alt="" className="rounded-lg shadow-2xl w-4/5 h-full" />
             <img src={parts} alt="" className="rounded-lg shadow-2xl absolute w-3/5 right-5 top-1/2 border-8" />
           </div>
           <div>
-            <p className="text-2xl text-orange-600 font-bold">About Us</p>
+            <p className="my-5 text-2xl text-orange-600 font-bold">About Us</p>
             <h1 className="text-5xl font-bold w-1/2">
               We are qualified & of experience in this field
             </h1>
---
+++ src/Pages/Home/Services/Services.js
@@ -0,0 +1,37 @@
+import React, { useEffect, useState } from "react";
+import ServicesCard from "./ServicesCard";
+
+const Services = () => {
+  const [services, setServices] = useState([]);
+  useEffect(() => {
+    fetch("services.json")
+      .then((res) => res.json())
+      .then((data) => setServices(data));
+  });
+  return (
+    <div>
+      <div className="text-center">
+        <p className="text-2xl font-bold text-orange my-2">Services</p>
+        <h2 className="text-5xl font-semi-bold my-2">Our Services Area</h2>
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Cupiditate,
+          molestias?
+        </p>
+      </div>
+      <div className="grid gap-6 grid-cols-1 md:grid-cols-2 lg:grid-cols-3">
+        
+        {
+            services.map(service => <ServicesCard 
+                
+                
+            key={service.service_id}
+            service ={service}
+            ></ServicesCard>)
+
+        }
+      </div>
+    </div>
+  );
+};
+
+export default Services;
